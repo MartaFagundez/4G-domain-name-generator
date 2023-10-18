cbe2dd24c89e138783d688403d7e59de83152cb1
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2023-10-18 22:02:31 +0000

    First version done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const pronoun = ["el", "nuestro", "tu", "un"];
+const adj = ["gran", "super", "famoso", "unico", "brillante", "nuevo"];
+const noun = ["mapache", "bus", "profe", "genio", "caballero"];
+const extensions = [".com", ".net", ".site"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+generateAllCombinations(pronoun, adj, noun, extensions);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateAllCombinations(arr1, arr2, arr3, arr4) {
+  const res = [];
+  for (const i of arr1) {
+    for (const j of arr2) {
+      for (const k of arr3) {
+        for (const l of arr4) {
+          res.push(`${i}${j}${k}${l}`);
+        }
+      }
+    }
+  }
+
+  console.log(
+    `Estas son las ${res.length} combinaciones posibles para tu dominio: `
+  );
+  for (const item of res) {
+    console.log(item);
+  }
+}
